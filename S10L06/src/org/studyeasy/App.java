9f76e5c0abaafbbbc13f7de731b4716c8518b39f
User <User@WINDOWS-1NP82FV>
2019-06-08 15:32:59 -0700

    Heoolworld

---
+++ S10L06/src/org/studyeasy/App.java
@@ -0,0 +1,20 @@
+package org.studyeasy;
+import java.util.ArrayList;
+public class App {
+	static ArrayList<String> ListName=new ArrayList<>();
+	public static void main(String[] args) {
+		ListName.add("Andile");
+		ListName.add("Spha");
+		ListName.add("Scelo");
+		ListName.add("Love");
+		ListName.add("Dad");
+		ListName.add("Mom");
+		ListName.add("Key");
+		ListName.add("Shark");
+		ListName.add("Can");
+		System.out.println(ListName);		
+		
+
+	}
+
+}
